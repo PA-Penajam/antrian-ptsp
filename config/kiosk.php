9195f49dca13495402e9e28f03def00cefc4d99d
papenajam <dataweb.papenajam@gmail.com>
2026-03-26 08:04:09 +0800

    Fix kiosk password env fallback

--- config/kiosk.php
+++ config/kiosk.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'kiosk_password' => env('KIOSK_PASSWORD'),
-    'tv_display_password' => env('TV_DISPLAY_PASSWORD'),
+    'kiosk_password' => env('KIOSK_PASSWORD', env('MODULE_PASSWORD')),
+    'tv_display_password' => env('TV_DISPLAY_PASSWORD', env('MODULE_PASSWORD')),
     'session_lifetime' => env('MODULE_SESSION_LIFETIME', 1440),
 ];
