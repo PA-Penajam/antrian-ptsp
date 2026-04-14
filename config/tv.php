74684c9d44a732b5f22fdc55da4b2332e20273cb
moohard <dian.kuba13@gmail.com>
2026-04-14 13:19:01 +0800

    fix: update default volume video ke 0.08 sesuai hasil testing

--- config/tv.php
+++ config/tv.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'video_volume' => (float) env('TV_VIDEO_VOLUME', 0.3),
-    'video_volume_during_tts' => (float) env('TV_VIDEO_VOLUME_DURING_TTS', 0.05),
+    'video_volume' => (float) env('TV_VIDEO_VOLUME', 0.08),
+    'video_volume_during_tts' => (float) env('TV_VIDEO_VOLUME_DURING_TTS', 0.01),
     'tts_volume' => (float) env('TV_TTS_VOLUME', 1.0),
 ];
